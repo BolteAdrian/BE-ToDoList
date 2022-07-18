0158652c70876ae45ea36c6eb4f8b3717d610d40
BolteAdrian <87446991+BolteAdrian@users.noreply.github.com>
2022-07-18 15:09:12 +0300

    Add files via upload

---
+++ Business access layer/Services/ServicePrincipalTask.cs
@@ -0,0 +1,156 @@
+﻿using Data_Access_Layer.Models;
+using Data_Access_Layer.Repositories;
+
+namespace Business_access_layer.Services
+{
+    public class ServicePrincipalTask
+    {
+        /// <summary>
+        /// we initialize a repository object to make the connection between the repository and the service
+        /// </summary>
+        public readonly RepositoryPrincipalTask _repository;
+        public ServicePrincipalTask(RepositoryPrincipalTask repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// This function will create a new task
+        /// </summary>
+        /// <param name="principalTask"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<string> AddTask(PrincipalTask principalTask)
+        {
+            try
+            {
+                if (principalTask == null)
+                {
+                    return "This task is empty";
+                }
+                else
+                {
+                    var DateNow = DateTime.Now;
+                    if (DateTime.Compare(principalTask.StartDate.Date, DateNow.Date) >= 0) {
+                        if (DateTime.Compare(principalTask.StartDate.Date, principalTask.EndDate.Date) <= 0)
+                        {
+                            await _repository.Create(principalTask);
+                            return "The task was created";
+                        }
+                        return "EndData can t be before the StartDate";
+                    }
+                    return "StartDate can t be before the the today s date";
+                }
+            }
+            catch (Exception e)
+            {   
+                return e.Message;
+            }
+        }
+
+       /// <summary>
+       ///This function will remove the task 
+       /// </summary>
+       /// <param name="Id"></param>Id of the task that we want to delete
+       /// <returns></returns>
+        public async Task<string> DeleteTask(int Id)
+        {
+            try
+            {
+                if (Id != 0)
+                {
+                    var obj = _repository.GetAll().Where(x => x.Id == Id).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        await _repository.Delete(obj);
+                        return "The task was removed";
+                    }
+                    return "The task doesn t exist";
+                }
+                return "The id is 0";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// This function will update the task
+        /// </summary>
+        /// <param name="principalTask"></param>The modified object
+        /// <returns></returns>
+        public async Task<string> UpdateTask(PrincipalTask principalTask)
+        {
+            try
+            {
+                if (principalTask != null)
+                {
+                    if (principalTask.Id != 0)
+                    {
+                        if (DateTime.Compare(principalTask.StartDate.Date, principalTask.EndDate.Date) <= 0)
+                        {
+                            await _repository.Update(principalTask);
+                            return "The task was modified";
+                        }
+                        else
+                        {
+                            return "EndData can t be before the StartDate";
+                        }
+                    }
+                    return "The id is null";
+                }
+                return "The task doesn t exist";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// Returneaza toate task-urile principale
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<PrincipalTask> GetAllTasks()
+        {
+            try
+            {
+                if (_repository.GetAll().ToList() != null)
+                {
+                    return _repository.GetAll().ToList();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returneaza un task in functie de id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public PrincipalTask GetTask(int id)
+        {
+            try
+            {
+                if (id != 0 || _repository.GetById(id)!=null)
+                {
+                    return _repository.GetById(id);
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Data Access Layer/Repositories/RepositoryPrincipalTask.cs
@@ -0,0 +1,165 @@
+﻿using Data_Access_Layer.Contacts;
+using Data_Access_Layer.Data;
+using Data_Access_Layer.Models;
+
+namespace Data_Access_Layer.Repositories
+{
+    public class RepositoryPrincipalTask : IRepository<PrincipalTask>
+    {
+        /// <summary>
+        ///  we initialize an object to make the connection between the repository and the data base
+        /// </summary>
+        private readonly masterContext _context;
+
+        public RepositoryPrincipalTask(masterContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// We add a new task in database
+        /// </summary>
+        /// <param name="principalTask"></param>the new object
+        /// <returns></returns>
+        public async Task<string> Create(PrincipalTask principalTask)
+        {
+            try
+            {
+                if (principalTask != null)
+                {
+                    var obj = _context.Add(principalTask);
+                    await _context.SaveChangesAsync();
+                    return "The task was created";
+                }
+                else
+                {
+                    return "The task is empty";
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// We remove a task from database
+        /// </summary>
+        /// <param name="principalTask"></param>the object that we want to delete
+        public async Task<string> Delete(PrincipalTask principalTask)
+        {
+            try
+            {
+                if (principalTask != null)
+                {
+                    var secondaryTasks = _context.SecondaryTasks.Where(s => s.PrincipalTaskId == principalTask.Id);
+
+                    foreach (var secondaryTask in secondaryTasks)
+                    {
+                        if (secondaryTask != null)
+                        {   _context.Remove(secondaryTask);
+                            await _context.SaveChangesAsync();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                        _context.Remove(principalTask);
+                        await _context.SaveChangesAsync();
+                        return "The task was deleted";
+                }
+                else
+                {
+                    return "The task doesn t exist";
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        /// <summary>
+        /// Return all the principal tasks from the database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<PrincipalTask> GetAll()
+        {
+            try
+            {
+                var obj = _context.PrincipalTasks.OrderByDescending(x=>x.Priority).ToList();
+                if (obj != null) return obj;
+                else return null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Return one task from the databse
+        /// </summary>
+        /// <param name="Id"></param>Id of the task that we want to get
+        /// <returns></returns>
+        public PrincipalTask GetById(int Id)
+        {
+            try
+            {
+                
+                    var Obj = _context.PrincipalTasks.FirstOrDefault(x => x.Id == Id);
+                if (Obj != null) { 
+                    return Obj;
+                }
+                else {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// This function will update the principal task and if the task property checked is changed to true,
+        /// than all the secondary tasks well have the property checked true
+        /// </summary>
+        /// <param name="principalTask"></param>the modified object
+        /// <returns></returns>
+        public async Task<string> Update(PrincipalTask principalTask)
+        {
+            try
+            {
+                if (principalTask != null)
+                {
+                    if(principalTask.Checked==true) {
+                        var secondaryTasks = _context.SecondaryTasks.Where(s => s.PrincipalTaskId == principalTask.Id).ToList();
+                        foreach(var secondaryTask in secondaryTasks)
+                        {
+                            if (secondaryTask != null)
+                            {
+                                secondaryTask.Checked = true;
+                                _context.Update(secondaryTask);
+                                _context.SaveChanges();
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    _context.Update(principalTask);
+                    await _context.SaveChangesAsync();
+                    return "The task was modified";
+                }
+                return "The task is invalid";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
